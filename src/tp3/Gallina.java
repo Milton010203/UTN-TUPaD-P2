ef41198b58f51dd90134e190016a4a29c1a0eb78
Milton010203 <miloalva12@gmail.com>
2025-09-26 13:27:27 -0300

    Se añade clase Gallina para TP3

---
+++ src/tp3/Gallina.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3;
+
+/**
+ *
+ * @author Milton
+ */
+public class Gallina {
+    private int idGallina;
+    private int edad;
+    private int huevosPuestos;
+
+    public Gallina(int idGallina, int edad) {
+        this.idGallina = idGallina;
+        this.edad = edad;
+        this.huevosPuestos = 0;
+    }
+
+    public void ponerHuevo() {
+        huevosPuestos++;
+    }
+
+    public void envejecer() {
+        edad++;
+    }
+
+    public void mostrarEstado() {
+        System.out.println("Gallina " + idGallina + ": Edad " + edad + " años, Huevos puestos: " + huevosPuestos);
+    }
+}
